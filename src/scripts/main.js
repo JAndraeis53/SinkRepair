8e505c17a8f5355f21dfc637469531cb6e879051
Jalon Duncan <duncanjalon@gmail.com>
2021-06-15 15:37:37 -0500

    first push

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,26 @@
+// import { SinkRepair } from "./SinkRepair.js"
+
+
+// const mainContainer = document.querySelector("#container")
+
+// const render = () => {
+//     mainContainer.innerHTML = SinkRepair()
+// }
+
+// render()
+
+import { fetchRequests } from "./dataAccess.js" // make sure the requests data has been fetched and set into application state first thing
 import { SinkRepair } from "./SinkRepair.js"
 
 
 const mainContainer = document.querySelector("#container")
 
 const render = () => {
-    mainContainer.innerHTML = SinkRepair()
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
 }
 
 render()
-
